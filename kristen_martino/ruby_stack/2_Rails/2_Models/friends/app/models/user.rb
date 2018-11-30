5cb2235931c93417a1e0811a16b2be8c916837ae
Kristen <29871623+pikap3w@users.noreply.github.com>
2018-11-30 15:03:53 -0600

    self-join queries

--- kristen_martino/ruby_stack/2_Rails/2_Models/friends/app/models/user.rb
+++ kristen_martino/ruby_stack/2_Rails/2_Models/friends/app/models/user.rb
@@ -4,3 +4,5 @@ class User < ApplicationRecord
   has_many :link_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :link_friends, :through => :link_friendships, :source => :user
 end
+
+User.where.not(id: User.first.friends)
